e8490226c13ef2a3424ea3ee832e9909c4cb8e79
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-08-12 16:00:37 +0700

    🌓 delete: unused codes

--- lib/presentation/page/start/start_page.dart
+++ lib/presentation/page/start/start_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:volcano/presentation/page/auth/sign_up_page.dart';
-import 'package:volcano/presentation/page/tutorial/tutorial_page.dart';
 import 'package:volcano/presentation/page/volcano/volcano_page.dart';
 import 'package:volcano/presentation/provider/back/auth/shared_preference.dart';
 import 'package:volcano/presentation/provider/global/is_done_tutorial.dart';
@@ -20,18 +19,7 @@ class StartPage extends HookConsumerWidget {
       child: Scaffold(
         body: authSharedPreference.isEmpty
             ? const SignUpPage()
-            : Stack(
-                children: [
-                  Opacity(
-                    opacity: ref.watch(isDoneTutorialProvider) ? 1 : 0.6,
-                    child: const VolcanoPage(),
-                  ),
-                  // TODO move tutoiral page to volcano page, when users tap the add todo from voice button we can suggest the tutorial
-                  !ref.watch(isDoneTutorialProvider)
-                      ? const TutorialPage()
-                      : const SizedBox(),
-                ],
-              ),
+            : const VolcanoPage(),
       ),
     );
   }
