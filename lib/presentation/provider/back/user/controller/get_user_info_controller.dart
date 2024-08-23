0b258549dd102ac773e291c0034e8a8557f1a0a8
aoiorio <130330538+aoiorio@users.noreply.github.com>
2024-08-23 23:52:44 +0900

    🌵 fix: user can't exit Volcano page when there's no connection of wifi

--- lib/presentation/provider/back/user/controller/get_user_info_controller.dart
+++ lib/presentation/provider/back/user/controller/get_user_info_controller.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:volcano/core/errors.dart';
 import 'package:volcano/domain/entity/user_info.dart';
 import 'package:volcano/presentation/component/global/custom_toast.dart';
+import 'package:volcano/presentation/page/dialogs/to_sign_up_dialog.dart';
 import 'package:volcano/presentation/provider/back/auth/shared_preference.dart';
 import 'package:volcano/presentation/provider/back/user/providers.dart';
 
@@ -16,7 +18,10 @@ class GetUserInfoController extends _$GetUserInfoController {
     return Either.left(BackEndError(statusCode: 100));
   }
 
-  void executeGetUserInfo({required FToast toast}) {
+  void executeGetUserInfo({
+    required FToast toast,
+    required BuildContext context,
+  }) {
     final token = ref.watch(authSharedPreferenceProvider);
     ref.read(userUseCaseProvider).executeGetUserInfo(token).then((value) {
       if (value.isRight()) {
@@ -26,7 +31,14 @@ class GetUserInfoController extends _$GetUserInfoController {
       } else {
         value.getLeft().fold(() => null, (error) {
           state = Either.left(error);
-          // TODO make a dialog to move to sign in page and show it here
+          // done make a dialog to move to sign in page and show it here
+          if (error.statusCode == null || error.statusCode == 404) {
+            // NOTE if the user access token is expired or something went wrong, I'll show you this dialog to go to sign up page
+            showToSignUpDialog(context,
+                messageToShow:
+                    'Something went wrong, please connect the wifi or sign in again.');
+            return;
+          }
           showToastMessage(
             toast,
             error.message!.detail ?? '😵‍💫 Something went wrong',
